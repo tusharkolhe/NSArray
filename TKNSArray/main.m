bb8b61d4b5498953836620792270489e5c0c9ea5
Tushar Kolhe <tushar.kolhe01@gmal.com>
2016-10-17 12:20:54 +0530

    Sorting Array

--- TKNSArray/main.m
+++ TKNSArray/main.m
@@ -30,8 +30,27 @@ int main(int argc, const char * argv[]) {
         if([Fruits isEqualToArray:Fruits1])
         {
             NSLog(@"Both Arrays are Same!!!!");
+        }else{
+            NSLog(@"We do not found Same content in both arrays");
         }
-
+        
+        
+        ////Sorting Array
+        
+        NSArray *germanMakes = @[@"Mercedes-Benz", @"BMW", @"Porsche",
+                                 @"Opel", @"Volkswagen", @"Audi"];
+        NSArray *sortedMakes = [germanMakes sortedArrayUsingComparator:
+                                ^NSComparisonResult(id obj1, id obj2) {
+                                    if ([obj1 length] < [obj2 length]) {
+                                        return NSOrderedAscending;
+                                    } else
+                                        if ([obj1 length] > [obj2 length]) {
+                                            return NSOrderedDescending;
+                                        } else {
+                                            return NSOrderedSame;
+                                        }
+                                }];
+        NSLog(@"%@", sortedMakes);
 
     }
     return 0;
